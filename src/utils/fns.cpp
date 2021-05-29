ae4fa255133b62496a24d045407ccd9aea59507d
melchor629 <melchor9000@gmail.com>
2021-05-29 19:29:14 +0200

    fix+feat: handle negative numbers in transform+unzip

--- src/utils/fns.cpp
+++ src/utils/fns.cpp
@@ -4,6 +4,75 @@
 
 namespace flac_bindings {
 
+  static int64_t readI32(const void* buff) {
+    return *((const int32_t*) buff);
+  }
+
+  static void writeI32(void* buff, int64_t value) {
+    *((int32_t*) buff) = value;
+  }
+
+  static int64_t readI24(const void* buff) {
+    const auto tmp = (const uint8_t*) buff;
+    const auto val = tmp[0] + (tmp[1] << 8) + (tmp[2] << 16);
+    return val | (val & 0x800000) * 0x1FE;
+  }
+
+  static void writeI24(void* buff, int64_t value) {
+    auto tmp = (uint8_t*) buff;
+    tmp[0] = value & 0xFF;
+    tmp[1] = (value >> 8) & 0xFF;
+    tmp[2] = (value >> 16) & 0xFF;
+  }
+
+  static int64_t readI16(const void* buff) {
+    return *((const int16_t*) buff);
+  }
+
+  static void writeI16(void* buff, int64_t value) {
+    *((int16_t*) buff) = value;
+  }
+
+  static int64_t readI8(const void* buff) {
+    return *((const int8_t*) buff);
+  }
+
+  static void writeI8(void* buff, int64_t value) {
+    *((int8_t*) buff) = value;
+  }
+
+  typedef int64_t (*BufferReader)(const void* buff);
+  static inline BufferReader getReader(uint64_t inBps, Napi::Env env) {
+    switch (inBps) {
+      case 1:
+        return readI8;
+      case 2:
+        return readI16;
+      case 3:
+        return readI24;
+      case 4:
+        return readI32;
+      default:
+        throw Napi::Error::New(env, "Unsupported "s + std::to_string(inBps) + " bits per sample"s);
+    }
+  }
+
+  typedef void (*BufferWriter)(void* buff, int64_t);
+  static inline BufferWriter getWriter(uint64_t outBps, Napi::Env env) {
+    switch (outBps) {
+      case 1:
+        return writeI8;
+      case 2:
+        return writeI16;
+      case 3:
+        return writeI24;
+      case 4:
+        return writeI32;
+      default:
+        throw Napi::Error::New(env, "Unsupported "s + std::to_string(outBps) + " bits per sample"s);
+    }
+  }
+
   static Napi::Value zipAudio(const Napi::CallbackInfo& info) {
     using namespace Napi;
     EscapableHandleScope scope(info.Env());
@@ -33,8 +102,15 @@ namespace flac_bindings {
     });
     auto channels = buffers.size();
 
+    if (samples == 0 || inBps == 0 || outBps == 0 || channels == 0) {
+      throw Error::New(info.Env(), "Invalid value for one of the given properties");
+    }
+
     // NOTE: if outBps > inBps means that after one sample copy, there is some bytes that need
     //       to be cleared up. To be faster, all memory is cleared when allocated.
+    //       the reader and writer functions are here to properly read and write ints.
+    auto reader = getReader(inBps, info.Env());
+    auto writer = getWriter(outBps, info.Env());
     uint64_t outputBufferSize = samples * outBps * channels;
     char* outputBuffer =
       (char*) (outBps <= inBps ? malloc(outputBufferSize) : calloc(outputBufferSize, 1));
@@ -42,7 +118,8 @@ namespace flac_bindings {
       for (uint64_t channel = 0; channel < channels; channel++) {
         char* out = outputBuffer + (sample * channels + channel) * outBps;
         char* in = buffers[channel] + sample * inBps;
-        memcpy(out, in, outBps);
+        auto value = reader(in);
+        writer(out, value);
       }
     }
 
@@ -50,6 +127,64 @@ namespace flac_bindings {
       Buffer<char>::New(info.Env(), outputBuffer, outputBufferSize, [](auto, auto data) {
         free(data);
       }));
+  }
+
+  static Napi::Value unzipAudio(const Napi::CallbackInfo& info) {
+    using namespace Napi;
+    EscapableHandleScope scope(info.Env());
+
+    if (!info[0].IsObject()) {
+      throw TypeError::New(info.Env(), "Expected first argument to be object");
+    }
+
+    auto obj = info[0].As<Object>();
+    auto inBps = maybeNumberFromJs<uint64_t>(obj.Get("inBps")).value_or(4);
+    auto outBps = maybeNumberFromJs<uint64_t>(obj.Get("outBps")).value_or(4);
+    auto channels = maybeNumberFromJs<uint64_t>(obj.Get("channels")).value_or(2);
+
+    auto bufferPair = pointer::fromBuffer<char>(obj.Get("buffer"));
+    auto buffer = std::get<0>(bufferPair);
+    uint64_t samples = maybeNumberFromJs<uint64_t>(obj.Get("samples"))
+                         .value_or(std::get<1>(bufferPair) / inBps / channels);
+
+    if (inBps == 0 || outBps == 0 || samples == 0 || channels == 0) {
+      throw Error::New(info.Env(), "Invalid value for one of the given properties");
+    }
+
+    if (std::get<1>(bufferPair) < samples * inBps * channels) {
+      throw RangeError::New(
+        info.Env(),
+        "Buffer has size "s + std::to_string(std::get<1>(bufferPair))
+          + " but expected to be at least "s + std::to_string(samples * inBps * channels));
+    }
+
+    // NOTE: see above function note about the outputBuffer
+    auto reader = getReader(inBps, info.Env());
+    auto writer = getWriter(outBps, info.Env());
+    uint64_t outputBufferSize = samples * outBps;
+    std::vector<char*> outputBuffers(channels);
+    for (uint64_t channel = 0; channel < channels; channel += 1) {
+      outputBuffers[channel] =
+        (char*) (outBps <= inBps ? malloc(outputBufferSize) : calloc(outputBufferSize, 1));
+    }
+    for (uint64_t sample = 0; sample < samples; sample++) {
+      for (uint64_t channel = 0; channel < channels; channel += 1) {
+        char* out = outputBuffers[channel] + sample * outBps;
+        char* in = buffer + (sample * channels + channel) * inBps;
+        auto value = reader(in);
+        writer(out, value);
+      }
+    }
+
+    auto array = Napi::Array::New(info.Env(), channels);
+    for (uint64_t channel = 0; channel < channels; channel += 1) {
+      array[channel] = Buffer<char>::New(
+        info.Env(),
+        outputBuffers[channel],
+        outputBufferSize,
+        [](auto, auto data) { free(data); });
+    }
+    return scope.Escape(array);
   }
 
   static Napi::Value convertSampleFormat(const Napi::CallbackInfo& info) {
@@ -73,6 +208,10 @@ namespace flac_bindings {
     uint64_t samples =
       maybeNumberFromJs<uint64_t>(obj.Get("samples")).value_or(std::get<1>(bufferPair) / inBps);
 
+    if (inBps == 0 || outBps == 0 || samples == 0) {
+      throw Error::New(info.Env(), "Invalid value for one of the given properties");
+    }
+
     if (std::get<1>(bufferPair) < samples * inBps) {
       throw RangeError::New(
         info.Env(),
@@ -81,13 +220,16 @@ namespace flac_bindings {
     }
 
     // NOTE: see above function note about the outputBuffer
+    auto reader = getReader(inBps, info.Env());
+    auto writer = getWriter(outBps, info.Env());
     uint64_t outputBufferSize = samples * outBps;
     char* outputBuffer =
       (char*) (outBps <= inBps ? malloc(outputBufferSize) : calloc(outputBufferSize, 1));
     for (uint64_t sample = 0; sample < samples; sample++) {
       char* out = outputBuffer + sample * outBps;
       char* in = buffer + sample * inBps;
-      memcpy(out, in, outBps);
+      auto value = reader(in);
+      writer(out, value);
     }
 
     return scope.Escape(
@@ -103,6 +245,7 @@ namespace flac_bindings {
     auto obj = Object::New(env);
     obj.DefineProperties({
       PropertyDescriptor::Function(env, obj, "zipAudio", zipAudio, napi_enumerable),
+      PropertyDescriptor::Function(env, obj, "unzipAudio", unzipAudio, napi_enumerable),
       PropertyDescriptor::Function(
         env,
         obj,
---
+++ test/fns.test.js
@@ -0,0 +1,356 @@
+const { api: { fns } } = require('../lib')
+
+describe('fns', () => {
+  describe('convertSampleFormat', () => {
+    it('throws if input is not object', () => {
+      expect(() => fns.convertSampleFormat(null)).toThrow()
+    })
+
+    it('throws if buffer size is smaller', () => {
+      expect(() => fns.convertSampleFormat({
+        buffer: Buffer.alloc(16),
+        inBps: 16,
+        samples: 2,
+      })).toThrow()
+    })
+
+    it('throws if 0 samples is passed', () => {
+      expect(() => fns.convertSampleFormat({
+        buffer: Buffer.alloc(0),
+        samples: 0,
+        inBps: 2,
+      })).toThrow()
+    })
+
+    it('returns same buffer if no conversion is required', () => {
+      const buffer = Buffer.alloc(4 * 2, 1)
+
+      const returnedBuffer = fns.convertSampleFormat({
+        buffer,
+        inBps: 2,
+        outBps: 2,
+      })
+
+      expect(returnedBuffer).toBe(buffer)
+    })
+
+    it('scales down properly', () => {
+      const buffer = Buffer.from([
+        1, 0, 0,
+        2, 0, 0,
+        3, 0, 0,
+      ])
+      const expectedBuffer = Buffer.from([
+        1, 0,
+        2, 0,
+        3, 0,
+      ])
+
+      const returnedBuffer = fns.convertSampleFormat({
+        buffer,
+        inBps: 3,
+        outBps: 2,
+      })
+
+      expect(returnedBuffer).toEqual(expectedBuffer)
+    })
+
+    it('scales up properly', () => {
+      const buffer = Buffer.from([
+        1, 0,
+        2, 0,
+        3, 0,
+        -1, -1,
+      ])
+      const expectedBuffer = Buffer.from([
+        1, 0, 0, 0,
+        2, 0, 0, 0,
+        3, 0, 0, 0,
+        0xFF, 0xFF, 0xFF, 0xFF,
+      ])
+
+      const returnedBuffer = fns.convertSampleFormat({
+        buffer,
+        inBps: 2,
+      })
+
+      expect(returnedBuffer).toEqual(expectedBuffer)
+    })
+
+    it('larger buffer with samples provided returns smaller buffer', () => {
+      const buffer = Buffer.from([
+        1, 0,
+        2, 0,
+        3, 0,
+      ])
+      const expectedBuffer = Buffer.from([
+        1, 0, 0, 0,
+        2, 0, 0, 0,
+      ])
+
+      const returnedBuffer = fns.convertSampleFormat({
+        buffer,
+        inBps: 2,
+        samples: 2,
+      })
+
+      expect(returnedBuffer).toEqual(expectedBuffer)
+    })
+  })
+
+  describe('zipAudio', () => {
+    it('throws if input is not object', () => {
+      expect(() => fns.zipAudio(null)).toThrow()
+    })
+
+    it('throws if any buffer is not a buffer', () => {
+      expect(() => fns.zipAudio({
+        buffers: [null],
+      })).toThrow()
+    })
+
+    it('throws if any buffer is smaller than expected', () => {
+      expect(() => fns.zipAudio({
+        buffers: [Buffer.alloc(4 * 2), Buffer.alloc(4 * 2)],
+        inBps: 3,
+        samples: 3,
+      })).toThrow()
+    })
+
+    it('throws if samples is 0', () => {
+      expect(() => fns.zipAudio({
+        buffers: [Buffer.alloc(4 * 2)],
+        inBps: 2,
+        samples: 0,
+      })).toThrow()
+    })
+
+    it('returns interleaved audio', () => {
+      const buffers = [
+        Buffer.from([
+          1, 0,
+          2, 0,
+          3, 0,
+        ]),
+        Buffer.from([
+          0, 1,
+          0, 2,
+          0, 3,
+        ]),
+      ]
+      const expectedBuffer = Buffer.from([
+        1, 0,
+        0, 1,
+        2, 0,
+        0, 2,
+        3, 0,
+        0, 3,
+      ])
+
+      const returnedBuffer = fns.zipAudio({
+        buffers,
+        samples: 3,
+        inBps: 2,
+        outBps: 2,
+      })
+
+      expect(returnedBuffer).toEqual(expectedBuffer)
+    })
+
+    it('returns interleaved audio scaled down', () => {
+      const buffers = [
+        Buffer.from([
+          1, 0, 0,
+          2, 0, 0,
+          3, 0, 0,
+        ]),
+        Buffer.from([
+          0, 1, 0,
+          0, 2, 0,
+          0, 3, 0,
+        ]),
+      ]
+      const expectedBuffer = Buffer.from([
+        1, 0,
+        0, 1,
+        2, 0,
+        0, 2,
+        3, 0,
+        0, 3,
+      ])
+
+      const returnedBuffer = fns.zipAudio({
+        buffers,
+        samples: 3,
+        inBps: 3,
+        outBps: 2,
+      })
+
+      expect(returnedBuffer).toEqual(expectedBuffer)
+    })
+
+    it('returns interleaved audio scaled up', () => {
+      const buffers = [
+        Buffer.from([
+          1, 0,
+          2, 0,
+          3, 0,
+          -1, -1,
+        ]),
+        Buffer.from([
+          0, 1,
+          0, 2,
+          0, 3,
+          -1, -1,
+        ]),
+      ]
+      const expectedBuffer = Buffer.from([
+        1, 0, 0,
+        0, 1, 0,
+        2, 0, 0,
+        0, 2, 0,
+        3, 0, 0,
+        0, 3, 0,
+        0xFF, 0xFF, 0xFF,
+        0xFF, 0xFF, 0xFF,
+      ])
+
+      const returnedBuffer = fns.zipAudio({
+        buffers,
+        samples: 4,
+        inBps: 2,
+        outBps: 3,
+      })
+
+      expect(returnedBuffer).toEqual(expectedBuffer)
+    })
+  })
+
+  describe('unzipAudio', () => {
+    it('throws if input is not object', () => {
+      expect(() => fns.unzipAudio(null)).toThrow()
+    })
+
+    it('throws if any buffer is not a buffer', () => {
+      expect(() => fns.unzipAudio({
+        buffer: null,
+      })).toThrow()
+    })
+
+    it('throws if buffer is smaller than expected', () => {
+      expect(() => fns.zipAudio({
+        buffer: Buffer.alloc(4 * 2),
+        inBps: 3,
+        samples: 3,
+      })).toThrow()
+    })
+
+    it('throws if samples is 0', () => {
+      expect(() => fns.zipAudio({
+        buffer: Buffer.alloc(4 * 2),
+        inBps: 2,
+        samples: 0,
+      })).toThrow()
+    })
+
+    it('returns interleaved audio', () => {
+      const buffer = Buffer.from([
+        1, 0,
+        0, 1,
+        2, 0,
+        0, 2,
+        3, 0,
+        0, 3,
+      ])
+      const expectedBuffers = [
+        Buffer.from([
+          1, 0,
+          2, 0,
+          3, 0,
+        ]),
+        Buffer.from([
+          0, 1,
+          0, 2,
+          0, 3,
+        ]),
+      ]
+
+      const returnedBuffer = fns.unzipAudio({
+        buffer,
+        samples: 3,
+        inBps: 2,
+        outBps: 2,
+      })
+
+      expect(returnedBuffer).toEqual(expectedBuffers)
+    })
+
+    it('returns interleaved audio scaled down', () => {
+      const buffer = Buffer.from([
+        1, 0, 0,
+        0, 1, 0,
+        2, 0, 0,
+        0, 2, 0,
+        3, 0, 0,
+        0, 3, 0,
+      ])
+      const expectedBuffers = [
+        Buffer.from([
+          1, 0,
+          2, 0,
+          3, 0,
+        ]),
+        Buffer.from([
+          0, 1,
+          0, 2,
+          0, 3,
+        ]),
+      ]
+
+      const returnedBuffer = fns.unzipAudio({
+        buffer,
+        samples: 3,
+        inBps: 3,
+        outBps: 2,
+      })
+
+      expect(returnedBuffer).toEqual(expectedBuffers)
+    })
+
+    it('returns interleaved audio scaled up', () => {
+      const buffer = Buffer.from([
+        1, 0,
+        0, 1,
+        2, 0,
+        0, 2,
+        3, 0,
+        0, 3,
+        0xFF, 0xFF,
+        0xFF, 0xFF,
+      ])
+      const expectedBuffers = [
+        Buffer.from([
+          1, 0, 0,
+          2, 0, 0,
+          3, 0, 0,
+          -1, -1, -1,
+        ]),
+        Buffer.from([
+          0, 1, 0,
+          0, 2, 0,
+          0, 3, 0,
+          -1, -1, -1,
+        ]),
+      ]
+
+      const returnedBuffer = fns.unzipAudio({
+        buffer,
+        samples: 4,
+        inBps: 2,
+        outBps: 3,
+      })
+
+      expect(returnedBuffer).toEqual(expectedBuffers)
+    })
+  })
+})
