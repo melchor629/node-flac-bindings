7d4d1f395ded3eed886074e3cd03ccecbc49f494
melchor629 <melchor9000@gmail.com>
2021-05-29 19:29:53 +0200

    refac: modify coverage script

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -2,24 +2,21 @@
 
 set -e
 
-export PATH="$PATH:$PWD/node_modules/.bin"
-
 if [[ -d coverage ]]; then
-    rm -rf coverage
+  rm -rf coverage
 fi
 mkdir coverage
 
 if [[ ! -z "$1" ]] && [[ "$1" = "rebuild" ]]; then
-    cmake-js clean
-    cmake-js build --debug --CDCOVERAGE=ON --CDFLAC_BINDINGS_USE_EXTERNAL_LIBRARY=ON
+  npx cmake-js clean
+  npx cmake-js build --debug --CDCOVERAGE=ON --CDFLAC_BINDINGS_USE_EXTERNAL_LIBRARY=ON
 fi
 
-nyc mocha --recursive
-nyc report --reporter=text-lcov > coverage/js.info
+npx jest --coverage
 
 echo Extracting coverage report from C++
 lcov -c --quiet --directory build/CMakeFiles/flac-bindings.dir/src --base-directory src -o coverage/cpp.info --no-external
 
 echo Generating html
 cat coverage/js.info coverage/cpp.info > coverage/combined.info
-genhtml --quiet -o coverage coverage/combined.info
+genhtml --quiet -o coverage/report coverage/combined.info
