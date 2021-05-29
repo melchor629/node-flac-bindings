cd8772d5a5dd639b2b79645d58b289119be4a03a
melchor629 <melchor9000@gmail.com>
2021-05-29 19:29:53 +0200

    refac: modify coverage script

--- scripts/flac-build.js
+++ scripts/flac-build.js
@@ -1,70 +1,80 @@
-const cp = require('child_process');
+const cp = require('child_process')
+const debug = require('debug')('flac:build')
 
 const envOpts = {
-    useExternalLibrary: typeof process.env.FLAC_BINDINGS_USE_EXTERNAL_LIBRARY === 'string',
-    pkgConfigPath: process.env.PKG_CONFIG_PATH,
-};
+  useExternalLibrary: typeof process.env.FLAC_BINDINGS_USE_EXTERNAL_LIBRARY === 'string',
+  pkgConfigPath: process.env.PKG_CONFIG_PATH,
+}
 
 const run = (command, pipe = true) => {
-    console.log(`> ${command}`);
-    const proc = cp.spawnSync(command, {
-        encoding: 'utf-8',
-        shell: true,
-        stdio: pipe ? 'inherit' : undefined,
-    });
-    if(proc.error) {
-        throw proc.error;
-    }
+  debug(`Running command ${command}`)
+  const proc = cp.spawnSync(command, {
+    encoding: 'utf-8',
+    shell: true,
+    stdio: pipe ? 'inherit' : undefined,
+  })
+  if (proc.error) {
+    throw proc.error
+  }
 
-    return proc;
-};
+  return proc
+}
 
 // from https://github.com/lovell/sharp/blob/master/lib/libvips.js
-const pkgConfigPath = function () {
-    if(process.platform !== 'win32') {
-        const brewPkgConfigPath = run(
-            'which brew >/dev/null 2>&1 && eval $(brew --env) && echo $PKG_CONFIG_LIBDIR',
-            false,
-        ).stdout || '';
-        return [ brewPkgConfigPath.trim(), envOpts.pkgConfigPath, '/usr/local/lib/pkgconfig', '/usr/lib/pkgconfig' ]
-            .filter((p) => !!p)
-            .join(':');
-    } else {
-        return '';
-    }
-};
+const pkgConfigPath = () => {
+  if (process.platform !== 'win32') {
+    debug('Retrieving pkg-config path')
+    const brewPkgConfigPath = run(
+      'which brew >/dev/null 2>&1 && eval $(brew --env) && echo $PKG_CONFIG_LIBDIR',
+      false,
+    ).stdout || ''
+    return [brewPkgConfigPath.trim(), envOpts.pkgConfigPath, '/usr/local/lib/pkgconfig', '/usr/lib/pkgconfig']
+      .filter((p) => !!p)
+      .join(':')
+  }
+  return ''
+}
 
 const hasGlobalInstalledFlac = () => {
-    if(process.platform !== 'win32') {
-        const flacVersion = (
-            run(
-                `PKG_CONFIG_PATH="${pkgConfigPath()}" pkg-config --modversion flac`,
-                false,
-            ).stdout || ''
-        ).trim();
-        if(!flacVersion) {
-            return false;
-        }
-
-        const [ major, minor ] = flacVersion.split('.').map((n) => parseInt(n, 10));
-        return major === 1 && minor >= 3;
+  if (process.platform !== 'win32') {
+    debug('Checking if libflac is available')
+    const flacVersion = (
+      run(
+        `PKG_CONFIG_PATH="${pkgConfigPath()}" pkg-config --modversion flac`,
+        false,
+      ).stdout || ''
+    ).trim()
+    if (!flacVersion) {
+      debug('No libflac found')
+      return false
     }
 
-    return false;
-};
+    debug(`libflac found with version ${flacVersion}`)
+    const [major, minor] = flacVersion.split('.').map((n) => parseInt(n, 10))
+    return major === 1 && minor >= 3
+  }
 
-if(!envOpts.useExternalLibrary) {
-    if(!run('prebuild-install -r napi').status) {
-        process.exit(0);
-    }
+  return false
 }
 
-if(envOpts.useExternalLibrary || hasGlobalInstalledFlac()) {
-    if(run('cmake-js configure --CDFLAC_BINDINGS_USE_EXTERNAL_LIBRARY=ON').status) {
-        process.exit(1);
-    }
+if (!envOpts.useExternalLibrary) {
+  debug('Trying to install from prebuilt package...')
+  if (!run('prebuild-install -r napi').status) {
+    debug('Installed succesfully')
+    process.exit(0)
+  }
 }
 
-if(run('cmake-js build').status) {
-    process.exit(1);
+if (envOpts.useExternalLibrary || hasGlobalInstalledFlac()) {
+  debug('Trying to compile bindings using external libflac...')
+  if (run('cmake-js configure --CDFLAC_BINDINGS_USE_EXTERNAL_LIBRARY=ON').status) {
+    process.exit(1)
+  } else {
+    process.exit(0)
+  }
+}
+
+debug('Trying to compile with built-in libflac...')
+if (run('cmake-js build').status) {
+  process.exit(1)
 }
