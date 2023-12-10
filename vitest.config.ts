12c98ef39b46dc43bd3ffe5483427195ae60f0be
melchor629 <melchor9000@gmail.com>
2023-12-10 12:04:55 +0100

    chore: update deps

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,20 +1,29 @@
 import { defineConfig } from 'vitest/config'
 
+const isCI = ['true', '1'].includes(process.env.CI || '')
+
 export default defineConfig({
   test: {
     environment: 'node',
     coverage: {
+      enabled: isCI,
       provider: 'v8',
+      include: ['lib'],
+      exclude: ['lib/index.cjs', '**/*.d.ts'],
       reporter: [
         'text',
         ['lcovonly', { file: 'js.info' }],
       ],
     },
     include: ['test/**/*.test.[jt]s'],
-    reporters: ['default', 'junit'],
+    reporters: isCI ? ['default', 'junit'] : ['default'],
     setupFiles: './test/helper/setup.js',
     globalSetup: './test/helper/global-setup.js',
     outputFile: 'tests.junit.xml',
-    useAtomics: true,
+    poolOptions: {
+      threads: {
+        useAtomics: true,
+      },
+    },
   },
 })
